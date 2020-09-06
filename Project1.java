999bde01a9c50e973c24701d778c4eeab8152972
katserafine <katarinaserafine99@gmail.com>
2020-09-05 21:56:36 -0500

    idk

--- Project1.java
+++ Project1.java
@@ -1,1 +1,73 @@
 //project 1 for 4348,  processes
+
+//First interpretation depending on the
+//  input there is  certaint outcome
+
+//there are mutliple lines of input and therefore 
+//  the program much loop through all lines of input
+
+
+//need to read a text file line by line in JAVA
+
+
+//need a timer to simulate a timed execution of inturpt handler
+public class project1 {
+
+    //this will need to change to take in files for inpu and output
+    public static void main(String[] args){
+
+   
+        bool moreLines = true;
+
+        int x;
+        int y;
+
+        int[] memoryArray = new int[2000];
+
+        while(moreLines == true){
+
+
+            //each instruction should be executed before the 
+            //  before the next instruction is fetched
+            // Read in line (execution)
+            // read in input (variable)
+            
+            //int input = readinline command
+            //int = readinline variable
+
+
+            //Two different variables X and Y exist
+            //Must account for this
+
+        
+
+            int input = 0;
+
+            Switch(input) {
+                case 1:
+                    //Load value
+
+                    break;
+                case 2:
+                //load address
+
+                    break;
+                
+                case 3:
+                    //load fromindex address
+                    //get from stack data structure (stack = index)
+
+                    break;
+
+                case 4:
+
+                    //load the value at address + x
+                    //
+                    break;
+                case 5:
+                    //load 
+
+            }
+        }
+    }
+}
